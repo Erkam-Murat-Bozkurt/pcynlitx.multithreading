8bf3104c9f15f57f1087b5e94932d151a39af06e
Erkam Murat Bozkurt <er.muratbozkurt@gmail.com>
2025-05-17 19:59:17 +0300

    Copyright statements added

--- thread_blocker.cpp
+++ thread_blocker.cpp
@@ -1,18 +1,31 @@
 
 
+/*
+
+Copyright [2025] [Erkam Murat Bozkurt]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
 
 #include "thread_blocker.hpp"
 
-pcynlitx::thread_blocker::thread_blocker()
-{
-
-};
+pcynlitx::thread_blocker::thread_blocker(){ };
 
 
 
-pcynlitx::thread_blocker::~thread_blocker(){
-
-};
+pcynlitx::thread_blocker::~thread_blocker(){ };
 
 
 
@@ -20,8 +33,6 @@ void pcynlitx::thread_blocker::Receive_Data_Holder(thread_data_holder * ptr){
 
      this->data_holder_pointer = ptr;
 }
-
-
 
 
 void pcynlitx::thread_blocker::stop(int Number, int Rescuer_Thread)
